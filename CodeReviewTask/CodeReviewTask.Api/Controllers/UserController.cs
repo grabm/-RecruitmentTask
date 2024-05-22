f11390be5bef0365228827d83b643aae195de4fb
Mateusz Grab <mateuszgrab89@gmail.com>
2024-05-22 09:04:24 +0200

    add user controller

--- CodeReviewTask/CodeReviewTask.Api/Controllers/UserController.cs
+++ CodeReviewTask/CodeReviewTask.Api/Controllers/UserController.cs
@@ -4,7 +4,7 @@ namespace CodeReviewTask.Api.Controllers
 {
     public class UserController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Delete()
         {
             return View();
         }
