490bd646f6d1224e4af2c9dea80556de5ba7c236
Mateusz Grab <mateuszgrab89@gmail.com>
2024-05-23 09:50:58 +0200

    add db

---
+++ CodeReviewTask/CodeReviewTask.Database/Contexts/CodeReviewTaskContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CodeReviewTask.Domain.Models.User;
+using Microsoft.Extensions.Options;
+
+namespace CodeReviewTask.Database.Contexts
+{
+    public class CodeReviewTaskContext : DbContext
+    {
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring (DbContextOptionsBuilder dbContextOptionsBuilder)
+        {
+            dbContextOptionsBuilder.UseSqlite("Data Source=users.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasKey(u => u.Id);
+                entity.Property(u => u.Name);
+            });
+        }
+    }
+}
